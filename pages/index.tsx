345728ca46da300ffa760f5063b0d74c4c8b7edd
mmtayyab <tayyabshahid597@gmail.com>
2024-03-18 12:57:42 +0500

    Loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,48 @@
+"use client";
+
 import { Inter } from "next/font/google";
 import Vapi from "@vapi-ai/web";
-
-const vapi = new Vapi(process.env.NEXT_PUBLIC_VAPI_PUBLIC_KEY || "");
-
-const inter = Inter({ subsets: ["latin"] });
-
-vapi.on("call-start", () => {
-  console.log("call start");
-});
-
-vapi.on("call-end", () => {
-  console.log("call end");
-});
-
-vapi.on("speech-start", () => {
-  console.log("speech start");
-});
-
-vapi.on("speech-end", () => {
-  console.log("speech end");
-});
-
-const handleStartClick = async () => {
-  await vapi.start(process.env.NEXT_PUBLIC_ASSISTANT_ID || "");
-};
-const handleStopClick = async () => {
-  await vapi.stop();
-};
+import { Spinner, divider } from "@nextui-org/react";
+import React, { useState } from "react";
+import { Button } from "@nextui-org/react";
 
 export default function Home() {
+  const vapi = new Vapi(process.env.NEXT_PUBLIC_VAPI_PUBLIC_KEY || "");
+
+  // const inter = Inter({ subsets: ["latin"] });
+
+  vapi.on("call-start", () => {
+    console.log("call start");
+    setLoading(false);
+  });
+
+  vapi.on("call-end", () => {
+    console.log("call end");
+  });
+
+  vapi.on("speech-start", () => {
+    console.log("speech start");
+  });
+
+  vapi.on("speech-end", () => {
+    console.log("speech end");
+  });
+
+  const handleStartClick = async () => {
+    setLoading(true);
+    await vapi.start(process.env.NEXT_PUBLIC_ASSISTANT_ID || "");
+  };
+  const handleStopClick = async () => {
+    await vapi.stop();
+  };
+
+  const [loading, setLoading] = useState(false);
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex text-black">
+        {loading && <div>Loading</div>}
         <button id="start-call" onClick={() => handleStartClick()}>
           Start Call
         </button>
